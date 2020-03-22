ccc247f99d767e6ea7b1be17ed2b99ec7c49f803
Ethan Post <ethan.post@gmail.com>
2020-03-21 18:17:10 -0700

    refactored plotting with a function

--- corona_analysis.py
+++ corona_analysis.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Mar 20 11:02:59 2020
 
-@author: ethan
+@author: Ethan
 """
 
 # This script plots COVID-19 time series data.
@@ -26,7 +26,7 @@ def get_country_total(df, country_label, states=None):
     formatted_dates = [dt.datetime.strptime(d,"%m/%d/%y").date() for d in date_list]
     country_rows = df.loc[df['Country/Region'] == country_label]  # extract rows for desired country
     if states:
-        country_rows = country_rows.loc[us_rows['Province/State'].isin(states)]  # extract rows with state names
+        country_rows = country_rows.loc[country_rows['Province/State'].isin(states)]  # extract rows with state names
         
     country_rows2 = country_rows.drop(['Province/State','Country/Region','Lat','Long'],axis=1)  # remove unwanted columns
     country_row_tot = country_rows2.apply(np.sum,axis=0)  # sum all the country values
@@ -35,6 +35,19 @@ def get_country_total(df, country_label, states=None):
     
     return out
     
+def plot_figs(data_lists):
+    plt.figure(1)
+#    plt.plot(data_lists['dates'], data_lists['country_total'])
+#    plt.xticks(rotation=45)
+#    plt.tight_layout()
+#    plt.semilogy(data_lists['dates'], data_lists['country_total'])
+    fig, ((ax1), (ax2)) = plt.subplots(nrows=2, sharex=True)
+    ax1.plot(data_lists['dates'], data_lists['country_total'], 'r')
+    ax2.semilogy(data_lists['dates'], data_lists['country_total'], 'b')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
 
 datadir = "/home/ethan/Documents/Code/COVID-19/input_data"
 date_dir = "2020-03-19"
@@ -42,42 +55,18 @@ fpath = os.path.join(datadir, date_dir, "time_series_19-covid-Deaths.csv")
 
 states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
-#us_data = []
-#with open(fpath) as csvfile:
-#    csvreader = csv.reader(csvfile, delimiter=',')
-#    header = next(csvreader)
-#    for row in csvreader:
-#        if row[0] == "California":
-#            ca_data = row[4:]
-#        if (row[0] in states) and (row[1] == "US"):
-#            us_data.append(row[4:])
-            
 df = pd.read_csv(fpath)  # extract DataFrame from csv file
-date_list = df.columns.values.tolist()[4:]  # convert DataFrame header to a list, and remove first 4 elements
-formatted_dates = [dt.datetime.strptime(d,"%m/%d/%y").date() for d in date_list]
-us_rows = df.loc[df['Country/Region'] == 'US']  # extract US rows
-us_rows = us_rows.loc[us_rows['Province/State'].isin(states)]  # extract rows with state names
-us_rows2 = us_rows.drop(['Province/State','Country/Region','Lat','Long'],axis=1)  # remove unwanted columns
-us_row_tot = us_rows2.apply(np.sum,axis=0)  # sum all the US values
-us_tot_list = us_row_tot.values.tolist()  # convert DataFrame to a list
 
-plt.figure(1)
-plt.plot(formatted_dates, us_tot_list)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.figure(2)
-plt.semilogy(formatted_dates, us_tot_list)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+data_lists = get_country_total(df, 'US', states)  # get dictionary of time list and country total list
 
-#us_matrix = np.array(us_data)
-#us_total = np.sum(us_matrix, axis=0)
+plot_figs(data_lists)
 
-#dates = header[4:]
-#formatted_dates = [dt.datetime.strptime(d,"%m/%d/%y").date() for d in dates]
-
-#plt.plot(formatted_dates, ca_data)
-#plt.plot(formatted_dates, us_total)
-#plt.show()
-        
+#plt.figure(1)
+#plt.plot(data_lists['dates'], data_lists['country_total'])
+#plt.xticks(rotation=45)
+#plt.tight_layout()
+#plt.figure(2)
+#plt.semilogy(data_lists['dates'], data_lists['country_total'])
+#plt.xticks(rotation=45)
+#plt.tight_layout()
+#plt.show()   
