05106185c39deaa78436f958876fdf83873fdb40
Ethan Post <ethan.post@gmail.com>
2020-03-22 22:52:43 -0700

    calculate per million rate, add grid lines

--- corona_analysis.py
+++ corona_analysis.py
@@ -49,12 +49,15 @@ def plot_figs(x, y, country_label):
     plt.tight_layout()
     plt.show()
 
-def plot_figs2(x,y,country_label,fign):
+def plot_figs2(x,y,country_label,fign,y_max):
     
     f, axarr = plt.subplots(2,1, sharex=True)
     axarr[0].plot(x, y)
-    axarr[0].set_title('COVID-19 deaths (' + country_label + ')')
+    axarr[0].set_title('COVID-19 deaths per million (' + country_label + ')')
+    axarr[0].grid(True, axis='y')
     axarr[1].semilogy(x, y)
+    axarr[1].set_ylim(.001, y_max)
+    axarr[1].grid(True, axis='y')
     
     for tick in axarr[1].get_xticklabels():
         tick.set_rotation(55)
@@ -70,19 +73,24 @@ fpath = os.path.join(datadir, date_dir, "time_series_19-covid-Deaths.csv")
 states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 #countries = ['US','Italy']
-country_dict = [{'country':'US','states':states},
-                {'country':'Canada','states':None},
-                {'country':'Italy','states':None}]
+country_dict = [{'country':'US','states':states,'population':327000000},
+                {'country':'Canada','states':None,'population':37600000},
+                {'country':'Italy','states':None,'population':60500000}]
 
 df = pd.read_csv(fpath)  # extract DataFrame from csv file
+
+y_max = 1000
 
 i = 1
 for c in country_dict:
     data_lists = get_country_total(df, c['country'], c['states'])  # get dictionary of time list and country total list
     x = data_lists['dates']
     y = data_lists['country_total']
-    start_index = 40
-    plot_figs2(x[start_index:], y[start_index:], c['country'],i)
+    y_arr = np.array(y)
+    y_arr = np.multiply(np.divide(y_arr,c['population']),1000000)  # deaths per million
+    y = list(y_arr)
+    start_index = 20
+    plot_figs2(x[start_index:], y[start_index:], c['country'],i,y_max)
     i = i + 1
 
 #data_lists = get_country_total(df, 'US', states)  # get dictionary of time list and country total list
